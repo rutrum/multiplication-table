a4047d6f2837699c7764cb42459291d192350e03
David Purdum <purdum41@gmail.com>
2019-03-14 22:47:46 -0400

    Add broken mod120 option

--- master.js
+++ master.js
@@ -81,12 +81,13 @@ function updateSettings() {
     settings.pairs = getFactorPairs()
     settings.showNumbers = form.showNumbers.checked
     settings.showTable = form.showTable.checked
-    settings.underLattice = form.mod0.checked
-    settings.firstFree = form.mod1.checked
-    settings.secondFree = form.mod2.checked
-    settings.thirdFree = form.mod6.checked
-    settings.fourthFree = form.mod12.checked
-    settings.fifthFree = form.mod60.checked
+    settings.mod0 = form.mod0.checked
+    settings.mod1 = form.mod1.checked
+    settings.mod2 = form.mod2.checked
+    settings.mod6 = form.mod6.checked
+    settings.mod12 = form.mod12.checked
+    settings.mod60 = form.mod60.checked
+    settings.mod120 = form.mod120.checked
 
     // If crop lattice and its not a prime number, that is no lattice
     if (form.cropLattice.checked && settings.pairs.length > 0) {
@@ -95,7 +96,7 @@ function updateSettings() {
         settings.height = settings.pairs[length - 1][0]
     }
 
-    settings.colored = [0, 0, 0, 0, 0, 0];
+    settings.colored = [0, 0, 0, 0, 0, 0, 0];
     initLi()
 
     // Reset table
@@ -106,12 +107,12 @@ function updateSettings() {
         justValues()
     }
 
-    for (let i = 5; i > 0; i--) {
+    for (let i = 6; i > 0; i--) {
         settings.colored[i - 1] += settings.colored[i]; 
     }
     let algs = document.querySelectorAll(".count")
-    let i = 0;
-    for (let i = 0; i < 6; i++) {
+    
+    for (let i = 0; i < settings.colored.length; i++) {
         if (algs[i].nextElementSibling.checked) {
             algs[i].textContent = settings.colored[i]
         } else {
@@ -128,6 +129,18 @@ function clearTable() {
 
 function initLi() {
     Gi = greatestIntegersInRow(60)
+
+    // settings.Li6 = []
+    // for (let i = 1; i <= 420; i++) {
+    //     settings.Li6.push(0);
+    //     for (let j = 1; j <= i; j++) {
+    //         if (i % j == 0) {
+    //             settings.Li6[i-1] = Math.max(settings.Li6[i-1], Gi[j - 1])
+    //         }
+    //     }
+    // }
+
+    console.log(settings.Li6)
 
     settings.Li5 = []
     for (let i = 1; i <= 60; i++) {
@@ -235,7 +248,22 @@ function getColorClass(row, col) {
                 Gi = greatestIntegersInRow(60)
                 // console.log(Gi)
 
-                if (settings.fifthFree) {
+                // if (settings.mod120) {
+
+                //     if (p % 420 == 0 && p > settings.Li6[419]) {
+                //         settings.colored[6]++
+                //         return "mod120"
+                //     }
+
+                //     for (let i = 1; i < 420; i++) {
+                //         if (p % 420 == i && p > settings.Li6[i-1]) {
+                //             settings.colored[6]++
+                //             return "mod120"
+                //         }
+                //     }
+                // }
+
+                if (settings.mod60) {
 
                     if (p % 60 == 0 && p > settings.Li5[59]) {
                         settings.colored[5]++
@@ -250,7 +278,7 @@ function getColorClass(row, col) {
                     }
                 }
 
-                if (settings.fourthFree) {
+                if (settings.mod12) {
                     
                     if (p % 12 == 0 && p > settings.Li4[11]) {
                         settings.colored[4]++
@@ -265,7 +293,7 @@ function getColorClass(row, col) {
                     }
                 }
 
-                if (settings.thirdFree) {
+                if (settings.mod6) {
 
                     if (p % 6 == 0 && p > settings.Li3[5]) {
                         settings.colored[3]++
@@ -280,7 +308,7 @@ function getColorClass(row, col) {
                     }
                 }
 
-                if (settings.secondFree) {
+                if (settings.mod2) {
 
                     if (p % 2 == 0 && p > settings.Li2[1]) {
                         // Even row
@@ -293,14 +321,14 @@ function getColorClass(row, col) {
                     }
                 }
 
-                if (settings.firstFree) {
+                if (settings.mod1) {
                     // Does first row free see it?
                     if (pairs.length > 0 && p + 1 > pairs[0][1]) {
                         settings.colored[1]++
                         return "mod1"
                     }
                 }
-                if (settings.underLattice) {
+                if (settings.mod0) {
                     settings.colored[0]++
                     return "mod0"
                 }
